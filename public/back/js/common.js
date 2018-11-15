872a0e5eb24927a04a53dd30dfe58b75d17033ed
kuangdeyun <1046202158@qq.com>
2018-11-15 23:55:46 +0800

    公共功能退出功能-登录拦截完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,6 @@
+
+
+
 // 进度条
 // 开启进度条
 // NProgress.start();
@@ -37,9 +40,51 @@ $(document).ajaxStop(function(){
 
 
 // // jQuery 入口函数， 等待 dom 结构加载完成之后， 就执行
-// $(function(){
+$(function(){
 
-//   // 公共的功能
-//   // 功能1：导航点击切换功能
+  // 公共的功能
+  // 功能1：导航点击切换功能
+  $(".lt_aside .category").click(function(){
+    // 让下一个兄弟元素切换显示隐藏
+    $(this).next().stop().slideToggle();
+  })
 
-// })
+  // 功能2： 左边菜单列表切换功能
+  $(".lt_topbar .icon_left").click(function(){
+
+    $(".lt_aside").toggleClass("hidemenu");
+
+    $(".lt_main").toggleClass("hidemenu");
+
+    $(".lt_topbar").toggleClass("hidemenu");
+  })
+   
+  // 功能3： 退出功能
+  $(".lt_topbar .icon_right").click(function(){
+    //  点击按钮， 显示模态框   
+    // $("#Modal").modal("show")  // 显示
+    // $("#Modal").modal("hide")  // 隐藏
+
+    $("#logoutModal").modal("show");
+  });
+
+  // 模态框的退出按钮点击事件
+  $("#logoutBtn").click(function(){
+    // location.href = "login.html";  // 不能直接跳转，需要向后台请求ajax请求
+    
+    // 发送ajax请求，让后台销毁当前用户的登录状态
+    $.ajax({
+      type: "get",
+      url: "/employee/employeeLogout",
+      dataType: "json",
+      success: function(info){
+        console.log(info);
+        if( info.success ){
+          location.href = "login.html"; 
+        }
+      }
+
+    })
+ 
+  })
+})
